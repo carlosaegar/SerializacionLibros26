bb16516dadf6b3b035dadc7253ce4ed1d86d765d
carlosaegar <carlos.saegar@educa.jcyl.es>
2026-02-18 10:59:14 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,61 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+/* Clases Libro
+Escritor(recibe un ArrayList y una ruta de archivos
+ Lector (Recibe un ArrayList y una ruta de archivo
+ Main
+    Alimenta Arraylist
+    Llama al metodo correspondiente de
+ */
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[]args){
+        String archivo = "libros.det";
+        ArrayList<Libro> lista = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        String opcion;
+
+        do {
+            System.out.println("Escribe el titulo del libro: ");
+            String titulo = sc.nextLine();
+
+            System.out.println("Escribe el Autor del libro: ");
+            String autor = sc.nextLine();
+
+            System.out.println("Escribe el Año del libro: ");
+            int anio = sc.nextInt();
+            sc.nextLine();
+
+            lista.add(new Libro(titulo,autor,anio));
+
+            System.out.println("Seguir (s/n)");
+            opcion= sc.nextLine();
+
+        }while (opcion.equalsIgnoreCase("s"));
+
+        Escritor escritor = new Escritor();
+        Lector lector = new Lector();
+
+        try{
+            escritor.guardar(lista,archivo);
+            System.out.println("Libros guardados");
+
+            ArrayList<Libro> libros = lector.leer(archivo);
+            System.out.println("Libros Leidos");
+
+            for (Libro l: libros){
+                System.out.println(l);
+            }
+        }catch (Exception e){
+            System.out.println("Error: " + e.getMessage());
+
+        }
+
+
+
+
+    }
+
 }
