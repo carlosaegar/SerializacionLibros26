b23070d58022a729527d976576c1257237f0aafe
carlosaegar <carlos.saegar@educa.jcyl.es>
2026-02-18 10:58:30 +0100

    Update Escritor.java

--- src/Escritor.java
+++ src/Escritor.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.List;
+import java.util.Objects;
 
 public class Escritor {
+    public void guardar(List<Libro>libros, String archivo) throws Exception{
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(archivo))){
+            oos.writeObject(libros);
+        }
+    }
+
 }
