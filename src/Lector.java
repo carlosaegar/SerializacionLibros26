191e3808192d52e6a5b8c6934751a17001b67ed5
carlosaegar <carlos.saegar@educa.jcyl.es>
2026-02-18 10:58:43 +0100

    Update Lector.java

--- src/Lector.java
+++ src/Lector.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
 
 public class Lector {
+    public ArrayList<Libro> leer (String archivo) throws Exception{
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(archivo))){
+            return (ArrayList<Libro> )ois.readObject();
+
+
+        }
+    }
 }
