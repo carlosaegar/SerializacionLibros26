62a176ae8c0a968f77c024dd20bc4dcd5d0fb2d1
carlosaegar <carlos.saegar@educa.jcyl.es>
2026-02-18 10:58:57 +0100

    Update Libro.java

--- src/Libro.java
+++ src/Libro.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
+import java.io.Serializable;
 
-public class Libro {
+public class Libro implements Serializable {
+    private String titulo;
+    private int autor;
+    private int anioPublicacion;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getAutor() {
+        return autor;
+    }
+
+    public void setAutor(int autor) {
+        this.autor = autor;
+    }
+
+    public int getAnioPublicacion() {
+        return anioPublicacion;
+    }
+
+    public void setAnioPublicacion(int anioPublicacion) {
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    public Libro(String titulo, String anioPublicacion, int autor) {
+        this.titulo = titulo;
+        this.anioPublicacion = anioPublicacion;
+        this.autor = autor;
+
+    }
+
+    @Override
+    public String toString() {
+        return "Libro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", anioPublicacion=" + anioPublicacion +
+                '}';
+    }
 }
